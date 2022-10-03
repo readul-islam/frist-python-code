acfe1a8e32eeabb64fe67955042611bdda8137d7
readul-islam <developer.readul@gmail.com>
2022-10-03 18:18:44 +0600

    python learn

---
+++ hellow.py
@@ -0,0 +1,37 @@
+import django
+import random;
+''''print('hellow)'''
+
+fruits = ['apple', 'orange', 'banana']
+
+x,y,z = fruits
+print(x)
+print(y)
+print(z)
+
+def myFun ():
+     x=10
+     print(x)
+myFun() 
+
+
+ 
+print(django.get_version())
+
+x = 'readul Islam is good boy {}'
+print(x.format(10))
+
+fristWordUpperCase = x.capitalize()
+
+print(fristWordUpperCase.format(', right'))
+
+print(x.casefold())
+print(x.center(10))
+
+print(x.expandtabs(20))
+friend = 'jubayer hossin , readul islam, jubayer hossin , jubayer hossin'
+print(friend.count('readul'))
+print(x.find('readul'))
+
+
+  
---
+++ practice.py
@@ -0,0 +1,30 @@
+# create function
+def myFun (a,b):
+      return a * b
+x = myFun(5,6)
+
+# create anonymous function
+y = lambda a,b : a ** b
+print(x)
+print(y(4,4))
+x ={
+      "name": "readul Islam"
+}
+x["age"] = 14
+
+print(x["name"])
+print(x)
+y={'readul','islam'}
+z= {'readul','mahamud'}
+b=y.intersection(z)
+print(b)
+if 10 > 11:
+      print('yes 11 is getterthan 10')
+      
+elif 10 == 11:
+      print('yes 10 is less than 11')
+else:
+      print("i don't know which one big")
+
+# shorthand for if condition 
+if 10==10:print('yes 10 is equal')
